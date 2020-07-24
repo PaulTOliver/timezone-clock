801a888fdb6ad342e826e8840aa294ee923d7cbf
Paul Oliver <paul.t.oliver.design@gmail.com>
2020-07-25 01:05:35 +0200

    Flask server and endpoints

---
+++ server/server.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from flask import abort, Flask, jsonify
+from flask_cors import CORS
+from pytz import common_timezones, timezone
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/zones/', methods=['GET'])
+def zones():
+    """ Return a list of common timezones """
+    return jsonify(common_timezones)
+
+
+@app.route('/time-at/<path:zone>', methods=['GET'])
+def time_at(zone):
+    """ Return current time at given timezone """
+    if zone not in common_timezones:
+        abort(404)
+
+    tzobj = timezone(zone)
+
+    return jsonify({
+        'zone': tzobj.zone,
+        'time': datetime.now(tzobj).strftime('%a %d %b %Y %H:%M:%S'),
+    })
